1b64305226ef6d922f613da5df9a6741dc8ed408
gablisboa <gablisboa1@gmail.com>
2024-04-17 17:02:15 -0300

    feat: every redirect with route scroll in top

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,10 @@ import { playgroundRoutes } from './routes/playground-routes'
 import { ServiceRoutes } from './routes/service-routes'
 
 const router = createRouter({
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  },
   mode: 'history',
   base: 'process.env.BASE_URL,routes,',
   history: createWebHistory(),
