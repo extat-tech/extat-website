c39e9ed487e84d7730b9971627b4d2a284203397
gablisboa <gablisboa1@gmail.com>
2024-04-16 21:56:53 -0300

    fix: change path name

--- src/router/routes/service-routes/index.js
+++ src/router/routes/service-routes/index.js
@@ -1,6 +1,6 @@
 /** @type {import('vue-router').RouteRecordRaw} */
 export const ServiceRoutes = {
-  path: 'consultoriaFinanceira',
+  path: '/consultoriaFinanceira',
   name: 'consultoriaFinanceira',
   component: () => import('../../../views/Service01/index.vue')
 }
