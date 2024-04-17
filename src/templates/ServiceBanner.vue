68369d7d088634e74f2f166909983eb102404b44
gablisboa <gablisboa1@gmail.com>
2024-04-17 08:57:32 -0300

    feat: refine responsive for ServiceBanner

--- src/templates/ServiceBanner.vue
+++ src/templates/ServiceBanner.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="bg-gray-900 px-8 pt-[152px] pb-24 xl:px-16">
-    <div class="flex flex-col gap-6 mb-12">
-      <h1 class="w-full lg:max-w-3xl text-4xl lg:text-6xl font-medium text-white">{{ title }}</h1>
-      <p class="text-xl w-full md:max-w-2xl text-brand-50/80">
-        {{ subtitle }}
-      </p>
+  <div class="bg-brand-950 flex justify-center px-8 pt-[152px] pb-24 xl:px-16">
+    <div class="max-w-7xl w-full">
+      <div class="flex flex-col gap-6 mb-12">
+        <h1 class="w-full lg:max-w-3xl text-4xl lg:text-6xl font-medium text-white">{{ title }}</h1>
+        <p class="text-xl w-full md:max-w-2xl text-brand-50/80">
+          {{ subtitle }}
+        </p>
+      </div>
+      <ScheduleCall />
     </div>
-    <ScheduleCall />
     
   </div>
 </template>
